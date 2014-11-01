20bca3dd7b8ed5344c550ac34be1534a239ef153
Joe Dunn <dunnjose@msn.com>
2014-11-01 16:32:30 -0400

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Dunn, Joseph on 11/1/14.
+//  Copyright (c) 2014 com.merck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this World
+//
+//  Created by Dunn, Joseph on 11/1/14.
+//  Copyright (c) 2014 com.merck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
